e7365aef834fdec58e9d1c02b699aec7ee5468d1
Yukesh-GIT <yukesh2arun@gmail.com>
2024-07-06 11:32:06 +0530

    6/7

--- front-end/src/Components/Assets/Data.jsx
+++ front-end/src/Components/Assets/Data.jsx
@@ -8,29 +8,29 @@ let data_products = [
         id: 1,
         name: "product 1",
         image: p1,
-        new_price: 100,
-        old_price: 200
+        new_price:  "₹"+550,
+        old_price:  "₹"+900
     },
     {
         id: 2,
         name: "product 2",
         image: p2,
-        new_price: 150,
-        old_price: 220
+        new_price: "₹"+650,
+        old_price: "₹"+1220
     },
     {
         id: 3,
         name: "product 3",
         image: p3,
-        new_price: 130,
-        old_price: 260
+        new_price: "₹"+1530,
+        old_price: "₹"+2260
     },
     {
         id: 4,
         name: "product 4",
         image: p4,
-        new_price: 140,
-        old_price: 290
+        new_price: "₹"+1540,
+        old_price: "₹"+2290
     },
 ];
 
--- front-end/src/Components/Items/Item.jsx
+++ front-end/src/Components/Items/Item.jsx
@@ -3,17 +3,23 @@ import './Item.css'
 
 const Item = (props) => {
   return (
-    <div className='item'>
-        <img src={props.image} alt="item" />
-        <p>{props.name}</p>
-        <div className="item-prices">
-            <div className="item-price-new">
-                <p>{props.new_price}</p>
-            </div>
-            <div className="item-price-old">
-                <p>{props.old_price}</p>
-            </div>
+    <div className='item-container' id='section-p1'>
+      <div className='item'>
+        <div className='img-box'>
+          <img src={props.image} alt="" />
         </div>
+        <div className='des'>
+          <h5>{props.name}</h5>
+          <div className="item-prices" >
+            <div className="item-price-old">
+              <p>{props.old_price}</p>
+            </div>
+            <div className="item-price-new">
+              <p>{props.new_price}</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
